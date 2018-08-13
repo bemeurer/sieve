39a3b736b28d54ae41bd32bb3ac637a6f2d6c39e
Bernardo Meurer <meurerbernardo@gmail.com>
2018-08-13 18:31:53 -0300

    Started implementing sieve stub

--- build.rs
+++ build.rs
@@ -5,16 +5,20 @@ use std::env;
 use std::fs;
 
 fn generate_cache_const() {
+    // Either get l1 cache size or default to 32 KiB
     #[cfg(feature = "optimize-cache")]
     let cache = cache_size::l1_cache_size().unwrap();
     #[cfg(not(feature = "optimize-cache"))]
     let cache = 32768_usize;
+    // Generate the code line
     let const_line = format!("pub const CACHE_SIZE: usize = {};", cache).to_string();
-
+    // Generate output file path
     let out = format!("{}/cache_size.rs", env::var("OUT_DIR").unwrap());
+    // Write out code
     fs::write(out, const_line).unwrap();
 }
 
 fn main() {
+    // Generate CACHE_SIZE const and lib.
     generate_cache_const();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
+extern crate num;
+
+use num::{Integer, Unsigned, FromPrimitive, CheckedAdd};
+use num::integer::{self, Roots};
+
 include!(concat!(env!("OUT_DIR"), "/cache_size.rs"));
+
+pub fn segsieve<T>(limit: T) -> Vec<T>
+where T: FromPrimitive + Integer + Unsigned + CheckedAdd + Roots + Clone
+{
+    let sqrt = integer::sqrt(limit.clone());
+    let cache_size = T::from_usize(CACHE_SIZE);
+    let segment_size = if cache_size.is_some() {
+        sqrt.max(cache_size.unwrap())
+    } else {
+        sqrt
+    };
+
+    let primes: Vec<T> = Vec::new();
+
+    for low in num::range_step(T::from_usize(0).unwrap(), limit, segment_size){
+        
+    }
+    unimplemented!()
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
+        assert!(!segsieve(100_usize).is_empty());
         assert_eq!(2 + 2, 4);
     }
 }
